fe9a8d0516ac98fac86eb83c07a51ae5f0717cac
Jitendra Singh Bisht <jitendrasinghbisht1404m@gmail.com>
2024-02-28 07:14:58 +0530

    little fixes

--- custom_videoPlayer/script.js
+++ custom_videoPlayer/script.js
@@ -19,17 +19,20 @@ pause.addEventListener('click',()=>{
 })
 audio.addEventListener('click',()=>{
     mute.style.zIndex='1'
+    volume.value=0
     video.muted = true;
     audio.style.zIndex='-1'
 })
 lowaudio.addEventListener('click',()=>{
     mute.style.zIndex='1'
+    volume.value=0
     video.muted = true;
     lowaudio.style.zIndex='-1'
 })
 mute.addEventListener('click',()=>{
     if(video.volume > 0.5) audio.style.zIndex='1'
     else lowaudio.style.zIndex='1'
+    volume.value=(video.volume)*100
     video.muted = false;
     mute.style.zIndex='-1'
 })
@@ -47,5 +50,35 @@ volume.addEventListener('change',()=>{
         lowaudio.style.zIndex='-1'
         mute.style.zIndex='1'
     }
+    video.muted = false;
     video.volume = (volume.value)/100
 })
+
+video.addEventListener('click',()=>{
+    if (video.paused){
+        pause.style.zIndex='1'
+        video.play()
+        play.style.zIndex='-1'
+    } else {
+        play.style.zIndex='1'
+        video.pause()
+        pause.style.zIndex='-1'
+    }
+})
+
+video.addEventListener('pause',(()=>{
+    pause.style.zIndex='-1'
+    play.style.zIndex='1'
+}))
+video.addEventListener('play',(()=>{
+    pause.style.zIndex='1'
+    play.style.zIndex='-1'
+    if (!(hasAudio(video))){
+        noaudio.style.zIndex='1'
+        volume.style.disable='true'
+}}))
+function hasAudio (video) {
+    return video.mozHasAudio ||
+    Boolean(video.webkitAudioDecodedByteCount) ||
+    Boolean(video.audioTracks && video.audioTracks.length);
+}
